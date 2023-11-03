df5b8a8d1cfc3c34c5c7fbc36950f95683012b52
JcSilverX <jcsilverx@gmail.com>
2023-11-03 13:35:43 -0400

    update: app.js

--- webtoonsx/static/assets/js/app.js
+++ webtoonsx/static/assets/js/app.js
@@ -1,6 +1,11 @@
 import './src/components/Carousel.js';
 import './src/components/Dropdown.js';
+import './src/components/Modal.js';
+import './src/components/Offcanvas.js';
+import './src/components/Toast.js';
 
 // webtoonsx
 import './src/components/webtoonsx/ColorModes.js';
 import './src/components/webtoonsx/DailyTabs.js';
+import './src/components/webtoonsx/Messages.js';
+import './src/components/webtoonsx/Search.js';
