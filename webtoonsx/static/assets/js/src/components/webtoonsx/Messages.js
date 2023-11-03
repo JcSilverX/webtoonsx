06038f8088e1b384be4e28f4ccd754fa22ad5a1f
JcSilverX <jcsilverx@gmail.com>
2023-11-03 17:38:16 -0400

    deleted:    Messages.js

--- webtoonsx/static/assets/js/src/components/webtoonsx/Messages.js
+++
@@ -1,9 +0,0 @@
-import Toast from '../Toast.js';
-import SelectorEngine from '../dom/SelectorEngine.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const messages = SelectorEngine.findOne('#messages');
-    if (!messages) return;
-
-    Toast.getOrCreateInstance(messages).show();
-});
