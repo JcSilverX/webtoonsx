40bb0398fc1c09a9a04c27aaa433965daae4342c
JcSilverX <jcsilverx@gmail.com>
2023-11-04 18:04:21 -0400

    modified:   user/models.py

--- user/models.py
+++ user/models.py
@@ -17,3 +17,17 @@ class Profile(models.Model):
         return self.user.username
 
 
+"""
+    create profile when new user signs up
+"""
+# @receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
