e8c89a6c1f110edaeb50d06de6e698fa50a003f9
JcSilverX <jcsilverx@gmail.com>
2023-11-11 10:16:28 -0500

    modified: series/urls.py

--- series/urls.py
+++ series/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('genres', views.genres, name='genres'),
     path('popular', views.popular, name='popular'),
+
+    path('publish', views.publish, name='publish'),
 ]
 
